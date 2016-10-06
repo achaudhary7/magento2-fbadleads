981a3d41c47b83c6cab152fe2479f2ee0737ebf6
Anup Chaudhary <achaudhary7@gmail.com>
2016-10-06 11:50:07 +0530

    Create webhook.php

---
+++ webhook.php
@@ -0,0 +1,20 @@
+<?php
+$challenge = $_REQUEST['hub_challenge'];
+$security_token = $_REQUEST['hub_verify_token'];
+if($security_token == 'Mplex1234511'){
+	echo $challenge;
+}
+$input = json_decode(file_get_contents('php://input'),true);
+
+error_log(print_r($input,true));
+//file_put_contents('/tmp/filename.txt', print_r($input, true));
+$entry_arr = $input['entry'][0]['changes'];
+$i =0;
+foreach($entry_arr as $entry){
+	$i++;
+	$leadgen_id = $entry['value']['leadgen_id'];
+	$ad_id = $entry['value']['ad_id'];
+	$form_id = $entry['value']['form_id'];
+	//exec("php /var/www/html/fbapp/get_leadgen_data.php ".$leadgen_id." ".$ad_id." ".$form_id." > /dev/null &");		
+	exec("php /var/www/html/magento2/get_leadgen_data.php ".$leadgen_id." ".$ad_id." ".$form_id." > /dev/null &");
+}
